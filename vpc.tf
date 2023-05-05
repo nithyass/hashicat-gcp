48d5042b52900c75fccefe6a48cb82c4b62df806
nithyass <nithya01.ravindran@gmail.com>
2023-05-05 20:56:05 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
 
 module "vpc" {
-    source  = "terraform-google-modules/network/google"
-    version = "~> 3.0"
+    source  = "nithyass/network/google"
+    version = "3.4.0"
 
     project_id   = var.project
     network_name = "example-vpc"
