b845f96f26cb82aa8fa38fba87b559312cce79b2
nithyass <nithya01.ravindran@gmail.com>
2023-05-05 21:30:45 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -3,11 +3,11 @@ module "network" {
   version = "3.4.0"
   # insert required variables here
   project_id   = var.project
-  network_name = "example-vpc"
-  routing_mode = "GLOBAL"
+  network_name = "nithya-vpc"
+  #routing_mode = "GLOBAL"
   subnets = [
          {
-            subnet_name           = "gaurav-subnet"
+            subnet_name           = "nithya-subnet"
             subnet_ip             = "10.100.10.0/24"
             subnet_region         = var.region
         }
